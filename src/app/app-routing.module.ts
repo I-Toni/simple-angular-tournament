a9908e3add653c8ab1a4533e20b7cba4099aa887
Ibrahim Tonifarah <itonifa1@westga.edu>
2019-06-18 10:11:24 -0400

    edited app-routing.module.ts file imports to include tournament directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HelloComponent } from './hello/hello.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { BracketsComponent } from './brackets/brackets.component';
+import { HelloComponent } from './tournament/hello/hello.component';
+import { RegistrationComponent } from './tournament/registration/registration.component';
+import { BracketsComponent } from './tournament/brackets/brackets.component';
 
 const routes: Routes = [
   { path: '', component: HelloComponent},
