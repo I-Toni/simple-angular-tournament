713fe420c9f96c27fbb772c3f324f7800e33032b
Ibrahim Tonifarah <itonifa1@westga.edu>
2019-06-18 09:55:43 -0400

    removed extra app folder and added appropriate routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { BracketsComponent } from './brackets/brackets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HelloComponent},
+  { path: 'registration', component: RegistrationComponent},
+  { path: 'brackets', component: BracketsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
