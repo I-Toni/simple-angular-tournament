e6af242a8bef95c21e3e2d8c0abf2fdf057a21ec
Ibrahim Tonifarah <itonifa1@westga.edu>
2019-06-20 02:48:35 -0400

    added forms module to registration spec ts file to pass all test

--- src/app/tournament/registration/registration.component.spec.ts
+++ src/app/tournament/registration/registration.component.spec.ts
@@ -1,13 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RosterService } from '../../roster.service';
 import { RegistrationComponent } from './registration.component';
+import { FormsModule } from '@angular/forms';
 
 describe('RegistrationComponent with Real Service', () => {
   let component: RegistrationComponent;
   let fixture: ComponentFixture<RegistrationComponent>;
+  let rosterService = RosterService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule],
       declarations: [ RegistrationComponent ],
       providers: [ RosterService ]
     })
@@ -19,7 +22,7 @@ describe('RegistrationComponent with Real Service', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
+  
   it('should create', () => {
     expect(component).toBeTruthy();
   });
