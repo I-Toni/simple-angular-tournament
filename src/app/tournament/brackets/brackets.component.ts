1d6f152fc57dda756be9e4f2e93018023e2347da
Ibrahim Tonifarah <itonifa1@westga.edu>
2019-06-20 06:18:21 -0400

    added field roundNumber to brackets component

--- src/app/tournament/brackets/brackets.component.ts
+++ src/app/tournament/brackets/brackets.component.ts
@@ -8,11 +8,16 @@ import { RosterService } from '../../roster.service';
 })
 export class BracketsComponent implements OnInit {
    public players: string[];
+   public roundNumber: number;
    
   constructor(private rosterService: RosterService) { }
-
+  
   ngOnInit() {
     this.players = this.rosterService.getContestants();
+    this.roundNumber = 1;
   }
 
+  completeRound() {
+    this.roundNumber += 1;
+  }
 }
